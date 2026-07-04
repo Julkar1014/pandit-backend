eee1ca5164796b7ba03e351906684c4bf3e5a43d
Julkar1014 <julkarna786t@gmail.com>
2026-07-04 11:18:58 +0530

    Fix server

--- src/server.js
+++ src/server.js
@@ -5,22 +5,16 @@ const pool = require("./config/db");
 
 const PORT = process.env.PORT || 5000;
 
-async function startServer() {
+(async () => {
   try {
-    const connection = await pool.getConnection();
-
-    console.log("✅ Connected to MySQL Database");
-
-    connection.release();
-
-    app.listen(PORT, "0.0.0.0", () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
-
-  } catch (error) {
-    console.error("❌ MySQL Connection Failed");
-    console.error(error.message);
+    await pool.query("SELECT 1");
+    console.log("✅ Database Connected");
+  } catch (err) {
+    console.error("❌ Database Error:");
+    console.error(err);
   }
-}
 
-startServer();
+  app.listen(PORT, "0.0.0.0", () => {
+    console.log(`🚀 Server running on ${PORT}`);
+  });
+})();
